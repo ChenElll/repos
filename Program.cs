9b1eb58c4a96cbb9cd478f9538820b60752e4e58
ChenElll <elgarian@g.jct.ac.il>
2024-04-14 16:12:01 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Do you want to delete the Outlook cache folder? (Y/N)");
+        string input = Console.ReadLine();
+
+        if (input.ToUpper() == "Y")
+        {
+            DeleteOutlookCache();
+        }
+        else
+        {
+            Console.WriteLine("Operation aborted.");
+        }
+    }
+
+    static void DeleteOutlookCache()
+    {
+        string outlookCacheFolderPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + @"\Microsoft\Outlook";
+        if (Directory.Exists(outlookCacheFolderPath))
+        {
+            Directory.Delete(outlookCacheFolderPath, true);
+            Console.WriteLine("Outlook cache folder deleted successfully.");
+        }
+        else
+        {
+            Console.WriteLine("Outlook cache folder not found.");
+        }
+    }
+}
